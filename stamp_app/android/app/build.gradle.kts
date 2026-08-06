c6c6a4fabaf4c34cbcdc658470a61f81870084e0
Paul M. Summitt <psummitt@gmail.com>
2026-08-06 17:16:25 -0500

    update

--- stamp_app/android/app/build.gradle.kts
+++ stamp_app/android/app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 
 android {
     namespace = "com.example.stamp_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
+    // Explicitly set buildToolsVersion to override any outdated defaults
+    buildToolsVersion = "36.0.0"
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -25,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
